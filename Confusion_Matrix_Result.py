ffc6b90d46c8d3e2e2212448bdb4ab2a428cd579
劉育維 <simon3458@gmail.com>
2018-11-20 09:25:47 +0800

    Add files via upload

---
+++ Confusion_Matrix_Result.py
@@ -0,0 +1,14 @@
+from sklearn import cross_validation, preprocessing, metrics
+from sklearn.metrics import classification_report
+from sklearn.metrics import accuracy_score, recall_score, precision_score, f1_score, confusion_matrix
+
+def CM_result(y_dev, y_pred,pos=1,report_digits=4):
+    accuracy = accuracy_score(y_dev, y_pred)
+    precision = precision_score(y_dev, y_pred,pos_label=pos)
+    recall = recall_score(y_dev, y_pred,pos_label=pos)
+    f1 = f1_score(y_dev, y_pred,pos_label=pos)
+    CM_matrix = confusion_matrix(y_dev, y_pred)
+    report = classification_report(y_dev, y_pred,digits=report_digits)
+    
+    print('Accuracy = ' + str(accuracy) +'\nPrecision = ' + str(precision) +'\nRecall = ' + str(recall))
+    return accuracy, precision, recall, f1, report, CM_matrix
